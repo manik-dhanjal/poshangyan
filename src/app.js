6674ca0460b9cef0926313d4e2b9d7c022a397de
manik21 <manikdhanjal217@gmail.com>
2020-12-18 01:15:07 +0530

    cart basic logic is done

--- src/app.js
+++ src/app.js
@@ -1,54 +1,29 @@
 import React from 'react'
 import {BrowserRouter as Router , Switch, Route } from 'react-router-dom'
-import {Provider} from "react-redux"
 import Home from "./components/template/home";
 import SingleCreative from "./components/template/single-creative"
 import Search from "./components/template/search"
+import Cart from './components/template/cart'
 import Layout from "./components/layout/layout"
 import About from "./components/template/about"
-import store from "./redux/store"
-import  logo from './assets/Images/logo.png'
-import favicon from './assets/Images/favicon.png'
-import Helmet from 'react-helmet'
+import {CartProvider} from "./components/context/cart.context"
 import './App.css'
 
 const App = () => {
     return (
-    // <Provider store={store}>    
-        <Router>
-            {/* <img src={logo}/>
-            <Helmet>
-                <meta charset="utf-8" />
-                <link rel="icon" href={favicon} />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="theme-color" content="#000000" />
-                <meta
-                name="description"
-                content="Use this repository to educate yourself on all the nutrition information. To keep yourself and your loved ones safe and healthy."
-                />
-                <meta property="og:url"                content="www.poshangyan.com" />
-                <meta property="og:type"               content="Landing page" />
-                <meta property="og:title"              content="Poshangyan" />
-                <meta property="og:description"        content="Use this repository to educate yourself on all the nutrition information. To keep yourself and your loved ones safe and healthy." />
-                <meta property="og:image"              content={logo} />
-                <meta name="twitter:title" content="Poshangyan"/>
-                <meta name="twitter:description" content="Use this repository to educate yourself on all the nutrition information. To keep yourself and your loved ones safe and healthy."/>
-                <meta name="twitter:image" content={logo}/>
-                <meta name="twitter:card" content="summary_large_image"/>
-                <link rel="apple-touch-icon" href={favicon} />
-                <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-                <title>Poshangyan</title>
-            </Helmet> */}
+     <CartProvider>
+          <Router>
             <Layout>
                 <Switch>
                     <Route exact path='/' component={Home} />
                     <Route exact path='/search' component={Search}/>
                     <Route exact path='/:theme/:title' component={SingleCreative}/>
                     <Route exact path='/about' component={About}/>
+                    <Route exact path='/cart' component={Cart}/>
                 </Switch>
-        </Layout>
+            </Layout>
         </Router>
-    // </Provider>
+     </CartProvider>
     )
 }
 
---
+++ src/components/context/cart.context.js
@@ -0,0 +1,59 @@
+import React ,{useState,useContext,useEffect} from 'react'
+
+const GetCartContext = React.createContext()
+const AddCartContext = React.createContext()
+const DeleteCartContext = React.createContext()
+const CheckItemInCart =  React.createContext()
+
+export function useCart() { return useContext( GetCartContext ) }
+export function useAddCart() { return useContext( AddCartContext ) }
+export function useDeleteCart() { return  useContext( DeleteCartContext ) }
+export function useCheckItemInCart () {return useContext( CheckItemInCart )}
+export const   CartProvider = ({children}) => {
+
+    const [cart,setCart] = useState([]);
+    useEffect(() => {
+
+        if (typeof(Storage) !== "undefined") {
+            const cartStore = localStorage.cartItem;
+            if(cartStore)
+                setCart( JSON.parse(cartStore) )
+
+        } else {
+           console.log('local storage is not supported error')
+        }  
+    }, [])
+    useEffect(() => {
+        console.log(cart)
+        if (typeof(Storage) !== "undefined") {
+            localStorage.cartItem = JSON.stringify(cart);
+        } else {
+           console.log('local storage is not supported error')
+        }  
+    }, [cart])
+    const addToCart = (post) =>{
+            if(cart.reduce( (( result,item )=> item._id===post._id?false:result),true))
+                setCart([...cart,post])
+    }
+    const deleteFromCart = (id) =>{
+
+        const temp = cart.reduce((finalArr,item)=> item._id!==id ? [...finalArr,item] : [...finalArr],[])
+        setCart(temp)
+    }
+    const checkItemInCart = (id) =>{
+        return cart.reduce( (( result,item )=> item._id===id?true:result),false)
+    }
+
+    return (
+        <GetCartContext.Provider value = {cart}>
+                <AddCartContext.Provider value = {addToCart}>
+                    <DeleteCartContext.Provider value = {deleteFromCart}>
+                        <CheckItemInCart.Provider value = {checkItemInCart}>
+                            {children}
+                        </CheckItemInCart.Provider>
+                    </DeleteCartContext.Provider>
+                </AddCartContext.Provider>
+        </GetCartContext.Provider>
+    )
+}
+
