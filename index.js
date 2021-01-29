3726e86e36988f27ca9a1255b3b52a50576ac3a9
anshajsharma <anshajkumarsharma@gmail.com>
2021-01-29 17:56:01 +0530

    A

--- index.js
+++ index.js
@@ -180,8 +180,76 @@ schedule.scheduleJob('30 11 * * 5', () => {
         })
 })
 console.log(new Date().toString())
-schedule.scheduleJob('5 12 * * 5', () => {
-    console.log({ yup: 'ok' })
+schedule.scheduleJob('* * * * 5', () => {
+    console.log(new Date().toString())
+})
+schedule.scheduleJob('23 12 * * 5', () => {
+    const { parse } = require('json2csv');
+    console.log(new Date().toString())
+
+    Post.find()
+        .then(res => {
+            // console.log(csv)
+            // console.log(res)
+            let data = [];
+            res.forEach((doc) => {
+                let temp = {
+                    downloadsCount: doc.downloadsCount,
+                    thumbBucket: doc.thumbBucket,
+                    mimetype: doc.mimetype,
+                    thumbLocation: doc.thumbLocation,
+                    label: doc.label,
+                    Bucket: doc.Bucket,
+                    postId: doc.postId,
+                    languages: doc.languages,
+                    showFileName: doc.showFileName,
+                    mediaType: doc.mediaType,
+                    thumbKey: doc.thumbKey,
+                    themes: doc.themes,
+                    Location: doc.Location,
+                    thumbkey: doc.thumbKey,
+                    Key: doc.Key,
+                    source: doc.source,
+                    targetAudience: doc.targetAudience,
+                }
+                data.push(temp);
+            });
+            const csv = parse(data, ["source", "Bucket"]);
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'poshangyan@gmail.com',
+                    pass: 'Anshaj@123'
+                }
+            });
+
+            var message = {
+                from: "sender@server.com",
+                to: "anshajkumarsharma@gmail.com",
+                subject: "Database Weekly Update dd",
+                html: `Updated database`,
+                attachments: [
+                    {
+                        filename: "updated_database.csv",
+                        content: csv,
+                    },
+                ]
+            };
+
+
+
+            transporter.sendMail(message, (error, info) => {
+                if (error) {
+                    console.log(error);
+                    console.log({ err: "Error in sending email...." });
+                } else {
+                    console.log({ message: "Email sucessfully sent.." });
+                }
+            });
+
+        }).catch(e => {
+            console.log({ err: "Error in sending email.... 2" });
+        })
 })
 // var a = require('./ASD.json')
 // const Post = require('./schema/postSchema')
