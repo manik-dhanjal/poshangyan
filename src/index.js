8cd035a7d440b1fb5e3533e13a389c8507334c2a
manik21 <manikdhanjal217@gmail.com>
2021-03-27 11:04:51 +0530

    backend url changed

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import './assets/fonts/Lato-Bold.ttf'
 import './assets/fonts/Lato-Regular.ttf'
 import './assets/fonts/Lato-Light.ttf'
 
-// axios.defaults.baseURL = "https://poshangyan-api.niti.gov.in";
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://poshangyan-api.niti.gov.in";
+// axios.defaults.baseURL = "http://localhost:8000";
 
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
