ce5912de0076f47760740beadb0745f89c3a2177
manik21 <manikdhanjal217@gmail.com>
2021-05-29 06:26:26 +0530

    db.congfig bug fix

--- config/db.config.js
+++ config/db.config.js
@@ -5,11 +5,13 @@ require("dotenv").config()
 const connectDB = async () => {
     try{
         const isNodeEnvDevelopment = process.env.NODE_ENV === 'Development'
-        const mongoDbURI =  isNodeEnvDevelopment?'mongodb://localhost:27017/poshan-gyan':process.env.MONGODB_URI;
+        const mongoDbURI =  process.env.MONGODB_URI;
+        console.log(mongoDbURI)
         const db = await mongoose.connect(mongoDbURI,{
                     useNewUrlParser: true, 
                     useUnifiedTopology: true
                 })
+
                 console.log(`MongoDb Connected: ${db.connection.host}`)
     }catch(error){
         console.log(`Error: ${error.message}`);
